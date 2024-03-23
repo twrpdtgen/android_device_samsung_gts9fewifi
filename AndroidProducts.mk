b083191b4ea4c91f666e26d821a046f60cd9d470
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2024-03-23 17:15:26 +0100

    gts9fewifi: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-X510 Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_gts9fewifi.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_gts9fewifi-user \
+    omni_gts9fewifi-userdebug \
+    omni_gts9fewifi-eng
